6064a5cb22f65e9ee277020641cefa0bec59b4d2
AAA <80318813+Danpun9@users.noreply.github.com>
2026-07-16 02:24:11 +0900

    [level 2] Title: 숫자의 표현, Time: 2.94 ms, Memory: 11.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ Python3/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -2,8 +2,8 @@ def solution(n):
     answer = 0
     
     for i in range(1, n+1):
-        total = i
-        x = i + 1
+        total = 0
+        x = i
         while total < n:
             total += x
             x += 1
